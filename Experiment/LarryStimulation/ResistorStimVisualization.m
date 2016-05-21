5f5693b8fee6a66cb3b7f1a33bd2b5a2503fe22b
David Caldwell <djcald@cs.washington.edu>
2016-05-21 14:15:43 -0700

    pushed on LarryStim ResistorStimVisualization
    Former-commit-id: 276a04312ba7654d7a9b304ff28ede79ebffce72
    Former-commit-id: 5d12e8322019efdb6403a48b6750507a473e4b54

---
+++ Experiment/LarryStimulation/ResistorStimVisualization.m
@@ -0,0 +1,48 @@
+%% Script to analyze the stim data we recorded 
+% Drag the file in that we want to load to the workspace
+
+recordData = Wave.data;
+stimData = Stim.data; 
+stimCurrent = Sing.data;
+
+%% for the one channel output case 
+% note the weird transients at beginning and end sometimes as system is
+% armed, disarmed, recording is turned on and off. 
+
+figure
+subplot(2,1,1)
+plot(recordData(:,1))
+title('Recorded at headbox')
+
+subplot(2,1,2)
+plot(stimData(:,1))
+title('Recorded at stimulator')
+
+%% for the three channel output case
+
+figure
+plot(recordData(:,1))
+title('Recorded at headbox')
+
+figure
+subplot(2,2,1)
+plot(stimData(:,1))
+title('Stim Channel 1') 
+
+subplot(2,2,2)
+plot(stimData(:,2))
+title('Stim Channel 2') 
+
+subplot(2,2,3)
+plot(stimData(:,3))
+title('Stim Channel 3') 
+
+subplot(2,2,4)
+plot(stimData(:,4))
+title('Stim Channel 4') 
+
+%%
+
+
+
+
